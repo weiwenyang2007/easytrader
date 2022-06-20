5190ed95e8cc1def0bfa2949f6a693873fc443e5
piginzoo <piginzoo@gmail.com>
2022-06-20 18:00:13 +0800

    refactor

--- easytrader/clienttrader/ths519_clienttrader.py
+++ easytrader/clienttrader/ths519_clienttrader.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 
-from easytrader.clienttrader.clienttrader import  BaseLoginClientTrader
+from easytrader.clienttrader.universal_clienttrader import UniversalClientTrader
 
 
-class THS519ClientTrader(BaseLoginClientTrader):
+class THS519ClientTrader(UniversalClientTrader):
     """
     同花顺v5.19.15.133的实现(xiadan.exe)
     和5.18的界面相比，发生了变化，需要重新子类化

0a1a2d5f12f6dd20c369d18765f58450b87c11c7
piginzoo <piginzoo@gmail.com>
2022-06-21 15:39:35 +0800

    bugfix

--- easytrader/__init__.py
+++ easytrader/__init__.py
@@ -2,10 +2,11 @@
 import urllib3
 
 from easytrader import exceptions
-from easytrader.api import use, follower
+# from easytrader.api import use, follower
+# from easytrader.api import use
 from easytrader.log import logger
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
-__version__ = "0.23.0"
-__author__ = "shidenggui"
+__version__ = "1.0.0"
+__author__ = "piginzoo"

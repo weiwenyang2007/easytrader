63aaac7e0193c471e8ab1cfd5f2d0a27f0314db2
AlbertYang2007 <albertyang2007@gmail.com>
2023-10-26 23:12:21 +0800

    fix a big big bug, add buy and sell

--- mySanityCheck.py
+++ mySanityCheck.py
@@ -253,12 +253,14 @@ def deal_with_easy_trade(balance_data):
             buy_item = check_buy_condition(balance_data, target_stock)
             if buy_item:
                 log.debug('buy_item is ' + str(buy_item))
-                #entrust_no = user.sell(buy_item['stock_id'], price=buy_item['buy_price'], amount=buy_item['buy_number'])        
+                entrust_no = user.sell(buy_item['stock_id'], price=buy_item['buy_price'], amount=buy_item['buy_number'])        
+                log.debug('buy result: ' + str(entrust_no))
                 
             sell_item = check_sell_condition(balance_data, target_stock)
             if sell_item:
                 log.debug('sell_item is ' + str(sell_item))   
-                #entrust_no = user.sell(sell_item['stock_id'], price=sell_item['sell_price'], amount=sell_item['sell_number'])        
+                entrust_no = user.sell(sell_item['stock_id'], price=sell_item['sell_price'], amount=sell_item['sell_number'])        
+                log.debug('sell result: ' + str(entrust_no))
             
         
         #Check Sell condiction and make Sell

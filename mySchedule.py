c136c3e0bab50d0d87f6a38c1be17163e852b8ca
AlbertYang2007 <albertyang2007@gmail.com>
2023-11-01 06:27:08 +0800

    add

--- mySchedule.py
+++ mySchedule.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import schedule
 import time
 import mySanityCheck as sanity
@@ -7,13 +8,17 @@ log = myLogger.setup_custom_logger(__name__)
 def my_easy_trade():
     try:
         log.info('my_easy_trade start')
-        balance_data = sanity.sanity_check()
-        if balance_data:
-            sanity.deal_with_easy_trade(balance_data)
+        current_time = datetime.now().strftime("%H:%M:%S")
+        if current_time >= '09:15:00' and current_time < '15:15:00':
+            balance_data = sanity.sanity_check()
+            if balance_data:
+                sanity.deal_with_easy_trade(balance_data)
+            else:
+                log.warn('sanity_result is false, skip deal_with_easy_trade')    
         else:
-            log.warn('sanity_result is false, skip deal_with_easy_trade')    
+            log.info('my_easy_trade not run due to time is outof trade')
             
-        log.info('my_easy_trade end')    
+        log.info('my_easy_trade end') 
             
     except Exception as ex:
         log.exception(ex)
--- mySingleRun.py
+++ mySingleRun.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import schedule
 import time
 import mySanityCheck as sanity
@@ -5,13 +6,17 @@ import myLog as myLogger
 log = myLogger.setup_custom_logger(__name__)
 
 def my_easy_trade():
-    try:
+    try:    
         log.info('my_easy_trade start')
-        balance_data = sanity.sanity_check()
-        if balance_data:
-            sanity.deal_with_easy_trade(balance_data)
+        current_time = datetime.now().strftime("%H:%M:%S")
+        if current_time >= '09:15:00' and current_time < '15:15:00':
+            balance_data = sanity.sanity_check()
+            if balance_data:
+                sanity.deal_with_easy_trade(balance_data)
+            else:
+                log.warn('sanity_result is false, skip deal_with_easy_trade')    
         else:
-            log.warn('sanity_result is false, skip deal_with_easy_trade')    
+            log.info('my_easy_trade not run due to time is outof trade')
             
         log.info('my_easy_trade end')    
             
